63f5a9c0909bad1368de1be655d0410398d39890
melkor@aleph.engineering <melkor@aleph.engineering>
2023-08-01 21:46:40 -0400

    #9 :traffic_light: ***Hero Section*** <br />
    :rotating_light: **Problem**
    # Problem, Task, Reason for CommitSolution
    
    :tools: **Solution**
    # Solution or List of Changes
    
    :bookmark_tabs: **Files Changed**
    # List of Modified Files
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import Typer from "../reusable/EXTyper/Typer";
 
 const Hero = () => {
+  const Herotexts = [
+    "Full Stack SoftWare Engineer",
+    "3D Web Developer",
+    "Indie Game Developer",
+    "Azure Cloud Solution Developer",
+    "Google Cloud Architecture",
+    "AWS Cloud Architecture",
+  ];
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#cfee4d]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div className="">
+          <h1 className="text-white font-bold text-[30px]">
+            Hi I'm <span className="text-[#cfee4d]">Alvaro. </span>
+          </h1>
+          <span className="mt-2 text-white-100 font-medium text-[20px]">
+            <Typer texts={Herotexts} />
+          </span>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-6 bottom-[5px] w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary box-shadow-md`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -60,7 +60,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl box-shadow-md`}
         >
           <ul className="list-none flex flex-col justify-end items-start gap-4">
             {navLinks.map((_link) => (
