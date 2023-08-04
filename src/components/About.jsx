bca2d9ccc44c0f170bd9aa6c35b2a2737c336c23
melkor@aleph.engineering <melkor@aleph.engineering>
2023-08-04 15:37:07 -0400

    #11 :traffic_light: ***Build About Section*** <br />
    :rotating_light: **Problem**
    # Problem, Task, Reason for CommitSolution
    
    :tools: **Solution**
    # Solution or List of Changes
    
    :bookmark_tabs: **Files Changed**
    # List of Modified Files
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCrad = (props) => {
+  const { index, title, icon } = props;
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Hello, I'm Alvaro Raul Martin Peraza, a passionate Full Stack Developer
+        with over 15 years of experience in the software development industry. I
+        have expertise in a wide range of technologies and frameworks, enabling
+        me to build robust and scalable solutions for diverse projects.
+      </motion.p>
 
-export default About
+      <div className="mt-20 flex flex-wrap  gap-10">
+        {services.map((service, index) => (
+          <ServiceCrad key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about") ;
