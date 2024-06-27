7feabf1c80368993f84b37bd65d6718f70073dd2
melkorAMP <melkor@aleph.engineering>
2024-06-27 19:12:24 -0400

    #36 Update Meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,51 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Exagon-Soft</title>
+        <meta name="title" content="Exagon-Soft" />
+        <meta
+          name="description"
+          content="Turning code into magic.
+Delivering Custom Software and Innovative Technology to Propel Your Business Forward"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.exagon-soft.com/" />
+        <meta property="og:title" content="Exagon-Soft" />
+        <meta
+          property="og:description"
+          content="Turning code into magic.
+Delivering Custom Software and Innovative Technology to Propel Your Business Forward"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.exagon-soft.com/" />
+        <meta property="twitter:title" content="Exagon-Soft" />
+        <meta
+          property="twitter:description"
+          content="Turning code into magic.
+Delivering Custom Software and Innovative Technology to Propel Your Business Forward"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
+        <meta
+          name="keywords"
+          content="software, developer, frontend, full stack, Next js, cloud"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="10 days" />
+        <meta name="author" content="Alvaro R Martin" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
